f13d1e3ec5b806694f9fdd781219a594d6148fbc
Benjamin Locher <blocher@gmail.com>
2016-04-03 17:32:15 -0400

    slide show changes, corrected gitignore

--- public/wp-content/themes/grace/index.php
+++ public/wp-content/themes/grace/index.php
@@ -3,6 +3,13 @@
 
 $data = Timber::get_context();
 
+    $args = array(
+        'post_type' => 'slide',
+        'order_by' => 'menu_order',
+        'order' => 'ASC',
+    );
+    $data['slides'] = Timber::get_posts($args);
+
 Timber::render('index.twig', $data);
 
 
