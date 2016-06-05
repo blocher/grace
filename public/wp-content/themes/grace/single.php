df3538bdc72ca74e27442b1bb7655d025ccf8be2
Benjamin Locher <blocher@gmail.com>
2016-06-04 22:35:39 -0400

    images

--- public/wp-content/themes/grace/single.php
+++ public/wp-content/themes/grace/single.php
@@ -4,6 +4,17 @@ $data = Timber::get_context();
 
 $data['post'] = new ExtendedTimberPost();
 
+if (has_post_thumbnail( $data['post']->ID )) {
+  $data['header_photo'] = $data['post']->get_thumbnail()->src('page_header');
+} else if (empty ($data['header_photo'])) {
+
+  $default_photos = get_field('default_page_headers','option');
+  $photo_number = rand(0, count($default_photos)-1);
+  $data['header_photo'] = $default_photos[$photo_number]['photo']['sizes']['page_header'];
+
+}
+
+
 Timber::render('single.twig', $data);
 
 
