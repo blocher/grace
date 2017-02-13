5d6f9dfdd52ec3d7c80cd138267364358895122a
Benjamin Locher <blocher@gmail.com>
2017-02-12 22:43:02 -0600

    search result page fixes

---
+++ public/wp-content/themes/graceepiscopal/404.php
@@ -0,0 +1,6 @@
+<?php
+
+  $data = Timber::get_context();
+
+  Timber::render('404.twig', $data);
+
--- public/wp-content/themes/graceepiscopal/single.php
+++ public/wp-content/themes/graceepiscopal/single.php
@@ -3,6 +3,29 @@
 $data = Timber::get_context();
 
 $data['post'] = new ExtendedTimberPost();
+
+$type = $data['post']->post_type;
+
+if ($type=='sermon' || $type=='grace_notes' || $type=='bulletin_insert' || $type=='other_publication') {
+
+    $pdf = get_field('pdf',$data['post']->ID);
+    if (is_array($pdf)) {
+      $pdf = $pdf['url'];
+    }
+    wp_redirect($pdf, 302);
+    die();
+}
+
+if ($type=='alert' || $type=='slide') {
+
+    global $wp_query;
+    $wp_query->set_404();
+    status_header( 404 );
+    get_template_part( 404 );
+    exit();
+
+}
+
 Timber::render('single.twig', $data);
 
 
