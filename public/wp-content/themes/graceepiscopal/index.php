ef37a2955c3c4547793e903912ac50b9f38e31e1
Benjamin Locher <blocher@gmail.com>
2017-02-20 11:30:15 -0500

    fixing menu order of slides

--- public/wp-content/themes/graceepiscopal/index.php
+++ public/wp-content/themes/graceepiscopal/index.php
@@ -10,7 +10,7 @@ $data = Timber::get_context();
 *****/
 $args = array(
     'post_type' => 'slide',
-    'order_by' => 'menu_order',
+    'orderby' => 'menu_order',
     'order' => 'ASC',
 );
 $data['slides'] = Timber::get_posts($args, 'ExtendedTimberPost');
