1b9dc0d0be6095ce0a1ff568a77635283d8cd1c0
Benjamin Locher <blocher@gmail.com>
2017-10-25 09:10:09 -0400

    core and plugin updates

--- public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitcc1755b1d09f77a063058c1ec45890e6::getLoader();
+return ComposerAutoloaderInit39accf58ff2126d48ca962648c7176c1::getLoader();
