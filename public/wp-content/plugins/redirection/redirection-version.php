c30bb69a05fbaa18b3b5064b75ce68f3332e7ede
Benjamin Locher <ben@engagedc.com>
2019-08-14 19:10:27 -0400

    plugin updates

--- public/wp-content/plugins/redirection/redirection-version.php
+++ public/wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.3' );
-define( 'REDIRECTION_BUILD', '360758b71dd62f46abf52e4615e7b875' );
+define( 'REDIRECTION_VERSION', '4.3.3' );
+define( 'REDIRECTION_BUILD', 'f44c5a22d08097e437d91ad12f79080b' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
